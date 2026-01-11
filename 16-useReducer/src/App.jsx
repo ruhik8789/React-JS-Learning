d1267be1272f7f31d8b15f7351fcc1d89bf9db36
Ruhi K <ruhik8789@gmail.com>
2026-01-11 10:08:50 +0530

    pushed the changes.

---
+++ 16-useReducer/src/App.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useReducer } from "react";
+
+const App = () => {
+  // const [count, setCount] = useState(0);
+
+  const initialState = { count: 0 };
+
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "increase":
+        return { count: state.count + 1 };
+      case "decrease":
+        return { count: state.count - 1 };
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log(state);
+  console.log(useReducer(reducer, initialState));
+
+  return (
+    <>
+      <h1>{state.count}</h1>
+      <button
+        onClick={() => {
+          dispatch({ type: "increase" });
+        }}
+      >
+        Increase
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "decrease" });
+        }}
+      >
+        Decrease
+      </button>
+    </>
+  );
+};
+
+export default App;
